1efc5c2c13aef1ae214447ca56a78f03e07ba7ac
Noximob <imoveisnox@gmail.com>
2025-10-21 22:18:42 -0300

    fix: Reescreve pÃ¡gina de imÃ³veis para corrigir erro de sintaxe

--- src/app/imoveis/page.tsx
+++ src/app/imoveis/page.tsx
@@ -179,6 +179,7 @@ export default function ImoveisPage() {
                   </div>
                 </div>
               </div>
+            </div>
           </div>
         </div>
       </div>
